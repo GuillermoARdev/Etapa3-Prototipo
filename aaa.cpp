8c5885634f8029578c6e60235997b391f710bb6d
GuillermoARdev <75824639@continental.edu.pe>
2025-06-17 21:11:24 -0500

    inicio

--- aaa.cpp
+++ aaa.cpp
@@ -1,1 +1,6 @@
-a
+#include <iostream>  // Incluye la biblioteca de entrada/salida
+
+int main() {
+    std::cout << "Hola, mundo!" << std::endl;  // Imprime el mensaje
+    return 0;  // Indica que el programa terminó correctamente
+}
